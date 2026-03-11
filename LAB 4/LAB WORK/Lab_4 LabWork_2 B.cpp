55ca3570100561b6ff708a93eb81f58acaf5fe4f
muhammadabdalabdalahmad-blip <muhammadabdalabdalahmad@gmail.com>
2026-03-11 09:27:32 +0500

    Add files via upload

---
+++ LAB 4/LAB WORK/Lab_4 LabWork_2 B.cpp
@@ -0,0 +1,44 @@
+// Name:  MUHAMMAD ABDAL AHMAD
+// Department: Electrical Engineering ( communication)
+// Registration No: BF25NWELE0701
+// Section: B
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade;
+
+    cout << "Enter your grade (A, B, C, D, F): ";
+    cin >> grade;
+
+		grade=toupper(grade);
+
+    switch(grade) {
+        case 'A':
+        cout << "Excellent";
+            break;
+
+        case 'B':
+         cout << "Very Good";
+            break;
+
+        case 'C':
+         cout << "Good";
+            break;
+
+        case 'D':
+         cout << "Pass";
+            break;
+
+        case 'F':
+         cout << "Fail";
+            break;
+
+        default:
+            cout << "Invalid Grade";
+    }
+
+    return 0;
+}
+
