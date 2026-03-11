7dca94ddad3ecaf7eea0671baf14c82fc6481c73
muhammadabdalabdalahmad-blip <muhammadabdalabdalahmad@gmail.com>
2026-03-11 08:56:37 +0500

    Add files via upload

---
+++ LAB 1/LAB WORK/LAB_01 LabWork_1.cpp
@@ -0,0 +1,17 @@
+// Name: MUHAMMAD ABDAL AHMAD
+// Department: Electrical Engineering (Communication)
+// Registration No: BF25NWELE0701
+// Section: B
+
+#include <iostream>   // Header file for input and output
+using namespace std;  // Using standard namespace
+
+int main() 
+{
+    // Printing Hello World on the screen
+    cout << "Hello World!";
+
+    return 0;  // Program ends here
+}
+
+
